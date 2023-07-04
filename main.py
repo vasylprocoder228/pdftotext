bb564f2298ecc4742e085cf261672b255ce9c9d8
vasylprocoder228 <138466218+vasylprocoder228@users.noreply.github.com>
2023-07-04 11:08:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
-import pdfplumber
-from fastapi import FastAPI
-from fastapi import HTTPException
-from PyPDF2 import PdfReader
+import io
+import pdf2image
+import pytesseract
+from PIL import Image
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -16,15 +17,21 @@ async def extract_text(url: str, numOfPage: int = 1):
     with open('temp.pdf', 'wb') as f:
         f.write(response.content)
 
-    # Open the PDF file and extract text
-    with pdfplumber.open('temp.pdf') as pdf:
-        if 0 <= numOfPage <= len(pdf.pages) - 1:
-            page = pdf.pages[numOfPage]
-            extracted_text = page.extract_text()
-            extracted_html = page.extract_text(xhtml=True)
-        else:
-            extracted_text = ''
-            extracted_html = ''
+    # Convert PDF pages to images
+    images = pdf2image.convert_from_path('temp.pdf')
+
+    # Extract text from the specified page
+    if 0 <= numOfPage <= len(images) - 1:
+        image = images[numOfPage]
+        image_data = io.BytesIO()
+        image.save(image_data, format='PNG')
+        image_data.seek(0)
+        extracted_text = pytesseract.image_to_string(Image.open(image_data))
+    else:
+        extracted_text = ''
+
+    # Convert extracted text to HTML
+    extracted_html = extracted_text.replace('\n', '<br>')
 
     # Delete the temporary PDF file
     # Comment out the following line if you want to keep the downloaded file
@@ -34,5 +41,5 @@ async def extract_text(url: str, numOfPage: int = 1):
     return {
         'text': extracted_text,
         'html': extracted_html,
-        'numberOfPages': len(pdf.pages)
+        'numberOfPages': len(images)
     }
