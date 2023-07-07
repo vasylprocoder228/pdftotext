54141733e54ef437b81bf297e756515dd822038f
vasylprocoder228 <138466218+vasylprocoder228@users.noreply.github.com>
2023-07-07 18:35:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
+from flask import Flask, request, jsonify
+import fitz
 import requests
-from fastapi import FastAPI
-from fastapi import HTTPException
-from PyPDF2 import PdfReader
+import base64
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.post('/extract_text')
-async def extract_text(url: str):
-    # Download the PDF file
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Failed to download PDF")
+@app.route('/convert-pdf-images', methods=['POST'])
+def convert_pdf_images():
+    #pdf_url = request.json.get('pdf_url')
+    pdf_url = "https://rhaindubai.com/wp-content/uploads/2022/12/rhain-new-year-menu.pdf"
+    response = requests.get(pdf_url)
+    pdf_bytes = response.content
+    pdf_file = fitz.open(stream=pdf_bytes, filetype="pdf")
+    page_nums = len(pdf_file)
+    images_list = []
+    totalLen = 0
+    base64_images = []
 
-    with open('temp.pdf', 'wb') as f:
-        f.write(response.content)
+    for page_num in range(page_nums):
+        page_content = pdf_file[page_num]
+        images_list.extend(page_content.get_images())
 
-    # Open the PDF file and extract text
-    with open('temp.pdf', 'rb') as f:
-        reader = PdfReader(f)
-        num_pages = len(reader.pages)
-        text = ''
-        for page in range(num_pages):
-            page_obj = reader.pages[page]
-            text += page_obj.extract_text()
+    for i, image in enumerate(images_list, start=1):
+        xref = image[0]
+        base_image = pdf_file.extract_image(xref)
+        image_bytes = base_image['image']
+        image_ext = base_image['ext']
+        image_name = str(i) + '.' + image_ext
+        base64_image = base64.b64encode(image_bytes).decode("utf-8")
+        base64_images.append(base64_image)
+        totalLen += len(base64_image)
 
-    # Delete the temporary PDF file
-    # Comment out the following line if you want to keep the downloaded file
-    import os
-    os.remove('temp.pdf')
+    result = {
+        'total_length': totalLen
+        #'base64_images': base64_images
+    }
 
-    return {'text': text}
+    return jsonify(result)
