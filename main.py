3515363ed3d8289118335754c3cf2aff011eaa36
vasylprocoder228 <138466218+vasylprocoder228@users.noreply.github.com>
2023-07-10 12:41:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import fitz
+import os
+from PIL import Image
+import base64
 import requests
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -26,7 +30,29 @@ async def extract_text(url: str):
 
     # Delete the temporary PDF file
     # Comment out the following line if you want to keep the downloaded file
-    import os
     os.remove('temp.pdf')
 
     return {'text': text}
+
+@app.post('/extract_files')
+async def extract_text(pdf_url: str):
+    # Step 2: Download the PDF file
+    response = requests.get(pdf_url)
+    pdf_bytes = response.content
+    pdf_file = fitz.open(stream=pdf_bytes, filetype="pdf")
+    page_nums = len(pdf_file)
+    images_list = []
+    base_list = []
+    totalLen = 0
+    for page_num in range(page_nums):
+        page_content = pdf_file[page_num]
+        images_list.extend(page_content.get_images())
+    for i, image in enumerate(images_list, start=1):
+        xref = image[0]
+        base_image = pdf_file.extract_image(xref)
+        image_bytes = base_image['image']
+        image_ext = base_image['ext']
+        image_name = str(i) + '.' + image_ext
+        base64_image = base64.b64encode(image_bytes).decode("utf-8")
+        base_list.append({"imageName":image_name,"base64":base64_image})
+    return("images", base_list)
