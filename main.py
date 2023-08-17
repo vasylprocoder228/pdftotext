36f54f8d7fb246e9636173e28c04c6e9ea5a81b0
vasylprocoder228 <138466218+vasylprocoder228@users.noreply.github.com>
2023-08-17 21:29:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ import networkx as nx
 app = FastAPI()
 
 @app.post('/extract_text')
+async def call_dp_login_api():
+    url = "https://dubaiproperties.my.site.com/services/apexrest/DPLogin"
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "SFusername":"eloquaintegrationuser@dpg.com.prod",
+        "SFpassword":"sfmcdp2022vWl88ybT0rO8jgRTQ5edwpSU"
+    }
+    
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+
+    # Always good to print out the status code for debugging
+    print(f"Status code: {response.status_code}")
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+@app.post('/extract_text')
 async def extract_text(pdf_url: str):
     response = requests.get(pdf_url)
     if response.status_code != 200:
